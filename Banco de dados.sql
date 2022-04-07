7f768fae6cc96350004b0b555ec2c97e4796dc0f
Agatha Louzada <100931506+agathalouzada@users.noreply.github.com>
2022-04-07 09:31:20 -0300

    godofedo

---
+++ Banco de dados.sql
@@ -0,0 +1,14 @@
+insert into pessoas
+(nome,idade,sexo,peso,altura,nacionalidade)
+values
+("Dolores",25,"f",50.52,1,52,"Mexicana"),
+("Godolores",8,"f",30.25,1.12,"Americana");
+
+select * from pessoas;
+
+insert into pessoas 
+(nome,idade,sexo,peso,altura,nacionalidade)
+values 
+ ("Godofredo",25,"m",50.62,1.52,"Brasileiro");
+ 
+ select * from pessoas;
